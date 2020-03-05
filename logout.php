c0366b30c92d1116b24b1d31793efccaf9c9a2ff
Stijn van_der_Made <2120804@talnet.nl>
2020-03-05 15:55:07 +0100

    Uitwerking van de exercise

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
-if(isset($_POST['submit']))
-{
+if(isset($_POST['submit'])) {
     unset($_COOKIE['logdin']);
     $deleted = setcookie('logdin', '', time() - 3600);
     header("Location: index.php");
